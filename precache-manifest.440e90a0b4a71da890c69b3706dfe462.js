4a9fe02231ee5fd8ded46bd91dfcf56724ee96a9
Xianda Wu <xwu2@cougars.ccis.edu>
2019-01-29 15:15:54 -0600

    Updates

---
+++ precache-manifest.440e90a0b4a71da890c69b3706dfe462.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b4cc01e1c1948aef42",
+    "url": "/YintatNg/Goal-pro.git/static/js/runtime~main.a3b4cc01.js"
+  },
+  {
+    "revision": "23e4daf6a48d91cd7f95",
+    "url": "/YintatNg/Goal-pro.git/static/js/main.23e4daf6.chunk.js"
+  },
+  {
+    "revision": "fe72a475d52c886e08bc",
+    "url": "/YintatNg/Goal-pro.git/static/js/1.fe72a475.chunk.js"
+  },
+  {
+    "revision": "d56081f09279a471f59621b86feaef26",
+    "url": "/YintatNg/Goal-pro.git/index.html"
+  }
+];
